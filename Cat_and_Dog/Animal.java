a093fd9889755b68b118afa18495ee5314985dd2
dmitryapanovich <167235946+dmitryapanovich@users.noreply.github.com>
2024-05-15 23:51:38 +0300

    Update Animal.java

--- Cat_and_Dog/Animal.java
+++ Cat_and_Dog/Animal.java
@@ -1,6 +1,6 @@
 public class Animal {
-    private final int MAX_RUN = 0;
-    private final int MAX_SWIM = 0;
+    private final int max_Run = 0;
+    private final int max_Swim = 0;
     protected String name;
     protected int swim;
     protected int run;
@@ -24,7 +24,7 @@ public class Animal {
     }
 
     protected void run(int distance) {
-        if ((run >= distance) && (run <= MAX_RUN)){
+        if ((run >= distance) && (run <= max_Run)){
             System.out.println("Nickname " + this.name + "  - пробежал " + distance + "м.");
         } else {
             System.out.println("Nickname " + this.name + " - не пробежал ");
@@ -32,7 +32,7 @@ public class Animal {
     }
 
     protected void swim(int distance) {
-        if ((swim >= distance) && (swim <= MAX_SWIM)) {
+        if ((swim >= distance) && (swim <= max_Swim)) {
             System.out.println("Nickname " + this.name + "   - проплыл " + distance + "м.");
         } else {
             System.out.println("Nickname " + this.name + " - не проплыл ");
